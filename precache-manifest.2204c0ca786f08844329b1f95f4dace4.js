398bfd1a79cd89d4902d287e2d1ba4bb82ed2dfd
benchstyle1 <47250962+canijke@users.noreply.github.com>
2020-10-25 22:54:20 +0300

    deploy

---
+++ precache-manifest.2204c0ca786f08844329b1f95f4dace4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "222c262b5d2d007cf02a",
+    "url": "/currency/css/app.1b404ee0.css"
+  },
+  {
+    "revision": "f4c26ef05fa5547c7304743874ea1ebd",
+    "url": "/currency/index.html"
+  },
+  {
+    "revision": "222c262b5d2d007cf02a",
+    "url": "/currency/js/app.184027a7.js"
+  },
+  {
+    "revision": "834dc225ec28d52329d4",
+    "url": "/currency/js/chunk-vendors.4b47b263.js"
+  },
+  {
+    "revision": "74e6a489c35a8e384bafb3ff72bb0501",
+    "url": "/currency/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/currency/robots.txt"
+  }
+]);
